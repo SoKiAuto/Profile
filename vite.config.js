302cc7946d98bde6422a56a94a480e3a7a44a401
Admin <solankikishan149@gmail.com>
2025-07-14 15:55:16 +0530

    Added Google verification file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Profile/', // this is CRITICAL for GitHub Pages
   plugins: [react()],
-  base: '/work/', // your GitHub repo name
 })
